44b52322168f4dd1d5753d236222740d24a5a62e
Charles Emrich <libertinistic@gmail.com>
2017-05-05 10:13:55 -0700

    add ability to count down from user number

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
 ///// Business Logic /////
-function countUp (number) {
+function count (number, direction) {
   var result = [];
 
-  for (var i = 1; i <= number; i++) {
-
-    if (i % 3 === 0 && i % 5 === 0) {
-      result.push("ping-pong")
-    } else if (i % 3 === 0) {
-      result.push("ping");
-    } else if (i % 5 === 0) {
-      result.push("pong");
-    } else {
-      result.push(i);
+  if (direction === "up") {
+    for (var i = 1; i <= number; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        result.push("ping-pong")
+      } else if (i % 3 === 0) {
+        result.push("ping");
+      } else if (i % 5 === 0) {
+        result.push("pong");
+      } else {
+        result.push(i);
+      }
+    }
+  } else if (direction === "down") {
+    for (var i = number; i >= 1; i--) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        result.push("ping-pong")
+      } else if (i % 3 === 0) {
+        result.push("ping");
+      } else if (i % 5 === 0) {
+        result.push("pong");
+      } else {
+        result.push(i);
+      }
     }
   }
+
   return result;
 }
 
@@ -21,8 +35,10 @@ function countUp (number) {
 $(function() {
   $("form.form-one").submit(function(event) {
     event.preventDefault();
-    var input = parseInt($("input#user-number").val());
+  });
 
-    $("#output").text(countUp(input));
+  $("button").click(function() {
+    var input = parseInt($("input#user-number").val());
+    $("#output").text(count(input, $(this)[0].name));
   });
 });
