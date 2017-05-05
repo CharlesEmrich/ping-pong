89902c1fa2116ad9df689f9b8cfb23e080c2714f
Charles Emrich <libertinistic@gmail.com>
2017-05-05 10:01:26 -0700

    add 'ping-pong' replacement for numbers divisible by both 3 and 5

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@ function countUp (number) {
   var result = [];
 
   for (var i = 1; i <= number; i++) {
-    if (i % 3 === 0) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("ping-pong")
+    } else if (i % 3 === 0) {
       result.push("ping");
     } else if (i % 5 === 0) {
       result.push("pong");
